a56a77a16d6635d3ac74480d3a13bb9aac92132f
darishenko <dar1shenko@yandex.com>
2022-10-29 17:55:31 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using SignalGenerator.Signal.SignalType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignalGenerator
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            HarmonicSignal signalWave = new HarmonicSignal(1, 0, 2);
+            double[] c = signalWave.GenerateSignalWaveDots(10, 8);
+            
+            for(int i=0; i<c.Length; i++)
+            {
+                Console.WriteLine(i+1 + "   " + c[i]);
+            }
+        }
+    }
+}
---
+++ Signal/SignalType/HarmonicSignal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SignalGenerator.Signal.SignalType
+{
+    class HarmonicSignal
+    {
+        public double amplitude;
+        public double phase;
+        public double frequency;
+
+        public HarmonicSignal(double amplitude, double phase, double frequency)
+        {
+            this.amplitude = amplitude;
+            this.phase = phase;
+            this.frequency = frequency;
+        }
+
+        public double[] GenerateSignalWaveDots(int time, int sampling)
+        {
+            int steps = time * sampling;
+            double samplingStep = (double)time / steps;
+            double[] signalWaveDots = new double[ steps +1 ];
+
+            for(int i=0; i < steps +1; i++)
+            {
+                signalWaveDots[i] = amplitude * Math.Cos(i * samplingStep * frequency + phase);
+                Console.WriteLine( i+1 +"   "+i * samplingStep);
+            }
+
+            return signalWaveDots;
+        }
+    }
+}
