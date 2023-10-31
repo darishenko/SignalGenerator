ccebfb35d152196abfba310b17f37d56088e4e02
DARYA <89320281+darishenko@users.noreply.github.com>
2023-11-01 00:33:10 +0300

    Implemented computing amplitude and phase spectra

---
+++ Spectrum/SpectrumComputer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SignalGenerator.Spectrum
+{
+    public class SpectrumComputer
+    {
+        public static double[] ComputeAmplitudeSpectrum(Complex[] spectrum)
+        {
+            var N = spectrum.Length;
+            var amplitudeSpectrum = new double[N];
+
+            for (var k = 0; k < N; k++) amplitudeSpectrum[k] = spectrum[k].Magnitude;
+
+            return amplitudeSpectrum;
+        }
+
+        public static double[] ComputePhaseSpectrum(Comlex[] spectrum)
+        {
+            var N = spectrum.Length;
+            var phaseSpectrum = new double[N];
+
+            for (var k = 0; k < N; k++) phaseSpectrum[k] = Math.Atan(spectrum[k].Imaginary / spectrum[k].Real);
+
+            return phaseSpectrum;
+        }
+    }
+}
